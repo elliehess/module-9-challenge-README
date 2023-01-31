a465601b9aa124570ac19f1f049e324d6cde654a
Ellie <ellie@Ellies-MBP.attlocal.net>
2023-01-31 13:29:09 -0800

    first test

---
+++ Develop/README.md
@@ -0,0 +1,42 @@
+# TEST
+
+## Description
+
+what how why
+
+## Table of Contents 
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+N/A
+
+## Usage
+
+read itt
+
+## Credits
+
+nah
+
+## Tests 
+
+nope
+
+## License
+
+MIT License
+
+## Badges
+
+yep
+
+## Questions 
+Any questions? Feel free to check out my work here:
+<a>@elliehess</a>
+Or you can reach out to me at my email address below:
+elhess03@gmail.com
--- Develop/index.js
+++ Develop/index.js
@@ -26,6 +26,16 @@ const promptUser = () => {
       message: 'How can users use this project?',
     },
     {
+        type: 'input',
+        name: 'contribute',
+        message: 'How can users contribute to your project?',
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'If you have written any tests for your project, list examples of how to run them'
+      },
+    {
       type: 'checkbox',
       name: 'license',
       message: 'Which license did you use?',
@@ -33,8 +43,8 @@ const promptUser = () => {
     },
     {
         type: 'input',
-        name: 'contribute',
-        message: 'How can users contribute to your project?',
+        name: 'badge',
+        message: 'Choose your badge',
       },
       {
         type: 'input',
@@ -49,6 +59,55 @@ const promptUser = () => {
     ]);
 };
 
+const generateREADME = ({ name, description, installation, usage, contribute, test, license, badge, github, email }) =>
+
+`# ${name}
+
+## Description
+
+${description}
+
+## Table of Contents 
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+
+${installation}
+
+## Usage
+
+${usage}
+
+## Contributing
+
+${contribute}
+
+## Tests 
+
+${test}
+
+## License
+
+${license}
+
+## Badges
+
+${badge}
+
+## Questions 
+
+Any questions? Feel free to check out my work here:
+[Click here for my profile](${github})
+
+Or you can reach out to me at my email address below:
+${email}
+`;
 // Function to initialize app
 const init = () => {
     promptUser()
